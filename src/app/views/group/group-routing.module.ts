ec003ec29e54316689a5e7a339125806750eaafb
Mathem Lucas <lucas.raggi@hotmail.com>
2018-03-08 14:40:42 -0300

    -Criado component Group(Turma)

---
+++ src/app/views/group/group-routing.module.ts
@@ -0,0 +1,29 @@
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { CommonModule } from '@angular/common';
+//
+// import { GroupListComponent } from './group-list/group-list.component';
+// import { GroupFormComponent } from './group-form/group-form.component';
+//
+// const routes: Routes = [
+//   {
+//     path: 'list',
+//     component: GroupListComponent,
+//   },
+//   {
+//     path: 'form',
+//     component: GroupFormComponent,
+//   }
+// ];
+//
+// @NgModule({
+//   imports: [
+//     CommonModule,
+//     RouterModule.forChild(routes)
+//   ],
+//   exports: [
+//     RouterModule
+//   ],
+//   declarations: []
+// })
+// export class GroupRoutingModule {}
